e4309924a0d9ab13ea288ac9805627bad55f4563
shyamsunder <shyamreddyjanga@gmail.com>
2022-04-29 11:41:13 +0530

    added one java file to add two numbers

---
+++ batch-9/addition.java
@@ -0,0 +1,13 @@
+class addition
+{
+public static void main(string args[]){
+        int num1,num2,res;
+        system.out.println("enter first number");
+        scanner s=new scanner(system.in);
+        num1=s.nextInt();
+        system.out,println("enter second number");
+        num2=s.nextInt();
+        res=num1+num2;
+        system.out.println("result is "+res);
+}
+}
